1541f2e9dc4a96a4b87ef3c523b817bbcb664624
mkomarov <mykyta_komarov_tp@bmc.com>
2020-02-20 10:23:33 +0200

    relative path

--- src/main/java/travelFood/view/Main.java
+++ src/main/java/travelFood/view/Main.java
@@ -4,6 +4,7 @@ import com.sun.javafx.application.LauncherImpl;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
 
 /**
  * Created by Никита on 06.07.2017.
@@ -12,8 +13,19 @@ public class Main {
     public static String pathToDBFromJar;
     public static void main(String[] args) throws IOException {
         pathToDBFromJar = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        pathToDBFromJar = URLDecoder.decode(pathToDBFromJar, "UTF-8");
         pathToDBFromJar = pathToDBFromJar.substring(1);
-        pathToDBFromJar = "jdbc:hsqldb:file:" + pathToDBFromJar.replace("classes/", "db/TravelFoodDB").replace("TravelFoodJar.jar", "db/TravelFoodDB");
+        String [] pathToDBFromJarSplited = pathToDBFromJar.split("/");
+        pathToDBFromJar = "";
+        for (String s : pathToDBFromJarSplited) {
+            if (!s.contains(".jar") && !s.equals("classes")) {
+                pathToDBFromJar += s + "/";
+            } else {
+                break;
+            }
+        }
+        pathToDBFromJar += "db/TravelFoodDB";
+        pathToDBFromJar = "jdbc:hsqldb:file:" + pathToDBFromJar;
         System.out.println("pathToDBFromJar - " + pathToDBFromJar);
         LauncherImpl.launchApplication(TFWindow.class, MyPreloader.class, args);
     }
